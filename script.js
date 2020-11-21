37ed260a5978c5f4ccc0398b937a1805bce33e09
jenry valdes <jenryval93@gmail.com>
2020-11-21 17:07:36 -0600

    added more code

--- script.js
+++ script.js
@@ -1,10 +1,76 @@
-// // DOM elements
-var generateBtn = document.getElementById("button");
-var generateLower = document.getElementById("lower");
-var generateUpper = document.getElementById("Upper");
-var generateNumber = document.getElementById("Number");
-var generateSymbols = document.getElementById("Symbles");
-var generateLength = document.getElementById("Length");
+var generateLower = document.getElementById("generateLower");
+var generateUpper = document.getElementById("generateUpper");
+var generateNumber = document.getElementById("generateNumber");
+var generateSymbols = document.getElementById("generateSymbols");
+
+var button = document.getElementById("generate")
+
+var UpperCaseCharCodes = CharacterValue(65, 90)
+var LowerCaseCharCodes = CharacterValue(97, 122)
+var NumberCaseCharCodes = CharacterValue(48, 57)
+var SymbolsCaseCharCodes = CharacterValue(35, 47).concat(
+  CharacterValue(58, 64))
+
+button.addEventListener("click", e => {
+  e.preventDefault()
+  var generateUpper = Prompt("do you want to include Upper Case Letters?");
+  var generateLower = Prompt("do you want to include Lower Case Letters?");
+  var generateSymbols = Prompt("do you want to include Symblos?");
+  var generateNumber = Prompt("do you want to include Numbers?");
+  var password = generatePassword(generateLower, generateUpper, generateNumber, generateSymbols);
+})
+
+function generatePassword(generateLower, generateUpper, generateNumber, generateSymbols) {
+  console.log(UpperCaseCharCodes)
+}
+
+function CharacterValue(low, high) { 
+  var array =[]
+  for (i = low; i<=high; i++){
+    array.push(i)
+  }
+  return array
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // DOM elements
+// var generateBtn = document.getElementById("button");
+// var generateLower = document.getElementById("lower");
+// var generateUpper = document.getElementById("Upper");
+// var generateNumber = document.getElementById("Number");
+// var generateSymbols = document.getElementById("Symbles");
 
 // // Write password to the #password input
 // function writePassword() {
@@ -15,40 +81,9 @@ var generateLength = document.getElementById("Length");
 
 // }
 
-var randomFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbols
-};
-
-// // Add event listener to Upper button
-generateBtn.addEventListener("click", function(){
-  var length =  +generateLength.value;
-  var hasLower = generateLower.checked;
-  console.log(hasLower)
-});
-  
-
-
-// random character funcions/numbers
-
-function getRandomLower(){
- return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
- }
-
- function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
- }
-
- function getRandomSymbols(){
-   var symbols = "!@#$%^&*(){}=+/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
- }
-console.log(getRandomSymbols())
-
-   
+// var randomFunctions = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbols
+// };
