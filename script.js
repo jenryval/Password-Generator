ae100ef3b86535857fb49f8ee4ecc281b9dce30a
jenry valdes <jenryval93@gmail.com>
2020-11-21 20:06:25 -0600

    code working

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function writePassword() {
 
 function generatePassword() {
 
-var chosen = [];
+var generate = [];
 
 // asking user password criteria
 var upperCase = confirm("Insert uppercase letters?");
@@ -30,14 +30,49 @@ var numbers = confirm("Insert numbers?");
 }
 
 // password length 
-var userLength = prompt ("How long would you like your password? Please choose between 8-128.");
+var userLength = prompt ("choose chracters between 8 and 128.");
 while ((userLength < 8) || (userLength > 128)){
-  alert("Please choose between 8-128.");
+  alert("Please choose between parameters.");
   userLength = prompt("How long would you like your password?");
 } 
-// array for pass criteria
+// Variables for characters
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var Symbols = ["\"","!","#","$","%","&","\'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
 var Number = ["0","1","2","3","4","5","6","7","8","9"];
 
+
+// if statement to generate
+if (upperCase) {
+  for(var i = 0; i < upperCase.length; i++ ){
+    generate.push(upperCase[i]);
+
+  }
+}
+if(lowerCase){
+  for(var i=0; i < lowerCase.length; i++){
+    generate.push(lowerCase[i]);
+  }
+}
+if(Symbols){
+  for(var i=0; i < Symbols.length; i++){
+    generate.push(Symbols[i]);
+  }
+}
+if(Number){
+  for(var i=0; i < Number.length; i++){
+    generate.push(Number[i]);
+  }
+}
+var finalResult = [];
+for(i=0; i < userLength; i++){
+  finalResult.push(generate[Math.floor(Math.random()*generate.length)]);
+}
+
+// return finalResult
+return(finalResult.join(""));
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
